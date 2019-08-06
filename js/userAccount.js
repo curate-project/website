edfda037307528f5afe2d7984d29af4821071d95
Cryptonious <trustincrypto@gmail.com>
2019-08-06 16:27:53 -0700

    updates to User Account page

--- js/userAccount.js
+++ js/userAccount.js
@@ -1,4 +1,17 @@
-window.onload = function() {
+window.onload = function () {
+
+    var accountAvatar = document.getElementsByClassName("account__avatar")[0];
+    var accountMenu = document.getElementsByClassName("account__menu")[0];
+    var avatarAngle = document.getElementsByClassName("account__avatar-angle")[0];
+    accountAvatar.addEventListener("click", function () {
+        accountMenu.classList.toggle("account__menu--show");
+        avatarAngle.classList.toggle("account__avatar-angle--show"); 
+    });
+
+    $('.account__menu-item--logout').on('click', function() {
+        window.sessionStorage.removeItem('user');
+        window.location = "../index.html";
+    });
 
     const user = JSON.parse(window.sessionStorage.getItem('user'));
     console.log(JSON.stringify(user));
